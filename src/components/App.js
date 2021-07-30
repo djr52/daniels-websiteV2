45b22691b4727605a286521e852883a189de335a
Daniel Ramirez <juanfantasma1@yahoo.com>
2021-07-30 02:24:47 -0400

    Added particles component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../styles/App.css';
 import {BrowserRouter} from 'react-router-dom';
 import Navbar from "./Navbar";
 
-import Particles from 'react-particles-js';
+import Particles from './ParticlesComponent';
 
 function App() {
   return (
@@ -11,41 +11,7 @@ function App() {
         <BrowserRouter>
           <Navbar/>
           <div className="container"> 
-          <Particles className="particles-js"
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 160,
-	            "density": {
-	                "enable": false
-	            }
-	        },
-	        "size": {
-	            "value": 10,
-	            "random": true
-	        },
-	        "move": {
-	            "direction": "bottom",
-	            "out_mode": "out"
-	        },
-	        "line_linked": {
-	            "enable": false
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onclick": {
-	                "enable": true,
-	                "mode": "remove"
-	            }
-	        },
-	        "modes": {
-	            "remove": {
-	                "particles_nb": 10
-	            }
-	        }
-	    }
-	}} />
+          <Particles/>
           </div>
         </BrowserRouter>
 
