ce97f4d0c356d123bd2a443854ef1a398090e129
Daniel Ramirez <juanfantasma1@yahoo.com>
2021-08-06 13:54:02 -0400

    Added views and project data

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
 import '../styles/App.css';
 
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Navbar from "./Navbar";
+
+import Home from '../views/Home';
+import About from '../views/About';
+import Projects from '../views/Projects';
+import Resume from '../views/Resume';
+import Contact from '../views/Contact';
 
 import Particles from './ParticlesComponent';
 
 function App() {
   return (
-    <div className="fill-window">
-        <BrowserRouter>
-          <Navbar/>
-          <div className="container"> 
-          <Particles/>
-          </div>
+    <div className="wrapper">
+    	<BrowserRouter>
+        	<Navbar/>
+          	<div className="container"> 
+          		<Particles/>
+				<Route path ="/" exact component={Home}/>
+				<Route path ="/about" component={About}/>
+				<Route path ="/projects" component={Projects}/>
+				<Route path ="/resume" component={Resume}/>
+				<Route path ="/contact" component={Contact}/>
+
+          	</div>
         </BrowserRouter>
 
     </div>
